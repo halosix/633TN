09868e0b44162c326a7b2222b5d01cffc6f9b4e5
halosix <pleione@halosix.net>
2020-11-10 01:05:50 -0600

    update to v1.21
        25ms seems best for relay stability; 10ms worked fine for short periods then locked
        beaconinterval changed to 60 seconds default
        changed variable names to more generic ones; this is preparatory to modularizing
        the code to accomodate future sensor types

--- micronode.ino
+++ micronode.ino
@@ -1,10 +1,17 @@
 /*  micronode 
+    
+    v1.21
+    09 NOV 20
+    4956/6012 (82%) 112
 
+    25ms seems best for relay stability; 10ms worked fine for short periods then locked
+    beaconinterval changed to 60 seconds default
+    changed variable names to more generic ones; this is preparatory to modularizing
+    the code to accomodate future sensor types
+    
     v1.2
     08 NOV 20
-
     4730/6012 80
-
     decreased transmission interval from 50ms to 10ms, still stable in operation
     will monitor, may revert to 25ms if any problems crop up
     shorter interval means relay spends less time waiting for complete message,
@@ -14,9 +21,7 @@
     
     v 1.1
     08 NOV 20
-
     4678/6012, 76 bytes global vars
-
     streamlined
     changed delay to variable, easier to change
     moved node ID for ease of use
@@ -24,24 +29,20 @@
     
     v 1.0
     31 OCT 20
-
     4644/6012 bytes, 74 bytes global variables
-
     ATtiny85, 433Mhz ASK TX, DHT11, and LDR
-
       ATtiny85 pin 0 to DHT11 OUT
       ATtiny85 pin 1 to ASK TX DATA
-      ATtiny85 pin 2 to voltage divider,
+      ATtiny85 pin 2 to LDR voltage divider,
       use 10K for general, 1K for bright
-
 */
 
 #include <DHT.h>
 #include <Manchester.h>
 
 int IDb = 140;                // change this to node ID
-int xmitspacing = 10;         // change this to desired intertransmission variable interval, in millis
-long beaconinterval = 10000;  // change this to desired beacon interval, in millis
+int xmitspacing = 25;         // change this to desired intertransmission variable interval, in millis
+long beaconinterval = 60000;  // change this to desired beacon interval, in millis
 
 int dhtpin = 0;
 int txpin = 1;
